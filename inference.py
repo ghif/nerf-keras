f23d8d58acc717ef39dbbcccb1a773478d64d2d8
Muhammad Ghifary <mghifary@192.168.0.113>
2025-06-08 18:47:03 +0700

    train h256

--- inference.py
+++ inference.py
@@ -2,13 +2,16 @@ import os
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
 import keras
+from keras import ops
 import tensorflow as tf
 import numpy as np
 import argparse
 import json
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import imageio.v2 as imageio
 
-from data_utils import split_data, create_dataset_pipeline
+from data_utils import split_data, create_dataset_pipeline, get_rays, render_flat_rays
 from models import create_nerf_model, render_rgb_depth
 
 parser = argparse.ArgumentParser()
@@ -107,38 +110,6 @@ train_rays_flat, train_t_vals = train_rays
 val_imgs, val_rays = next(iter(val_ds))
 val_rays_flat, val_t_vals = val_rays
 
-# # Test ReLU directly
-# relu_layer_test = keras.layers.ReLU()
-# test_input_neg = tf.constant([-3.0, -0.5, 0.0, 0.5, 3.0])
-# test_output_neg = relu_layer_test(test_input_neg)
-# print(f"Direct ReLU test input: {test_input_neg.numpy()}")
-# print(f"Direct ReLU test output: {test_output_neg.numpy()}") # Expected: [0. 0. 0. 0.5 3.]
-
-# # Test ReLU in a minimal Sequential model
-# simple_model = keras.Sequential([
-#     keras.Input(shape=(1,)), # Expects input shape (batch_size, 1)
-#     # keras.layers.Dense(1, kernel_initializer='ones', bias_initializer='zeros'),
-#     keras.layers.Dense(1),
-#     keras.layers.ReLU()
-# ])
-# # Prepare input with correct rank for Dense layer
-# test_input_for_model = np.array([[-3.0], [-0.5], [0.0], [0.5], [3.0]], dtype=np.float32)
-# simple_output = simple_model.predict(test_input_for_model)
-# print(f"Simple model (Dense -> ReLU) input: {test_input_for_model.flatten()}")
-# print(f"Simple model (Dense -> ReLU) output: {simple_output.flatten()}") # Expected: [0. 0. 0. 0.5 3.]
-
-# mini_model_prerelu = keras.Model(nerf_model.input, nerf_model.layers[1].output)
-# print(mini_model_prerelu.summary(expand_nested=True))
-# h1 = mini_model_prerelu(val_rays_flat)
-# print(f"h1[0]: {h1[0]}")
-
-# mini_model = keras.Model(nerf_model.input, nerf_model.layers[2].output)
-# print(mini_model.summary(expand_nested=True))
-# h2 = mini_model(val_rays_flat.numpy())
-# print(f"h2[0]: {h2[0]}")
-
-# print(f"h1[0] == h2[0]: {np.allclose(h1[0], h2[0])}") 
-
 test_recons_images, depth_maps = render_rgb_depth(
     model=nerf_model,
     rays_flat=val_rays_flat,
@@ -167,3 +138,99 @@ for ax, ori_img, recons_img, depth_map in zip(
     ax[2].set_title("Depth Map")
 
 plt.show()
+
+
+# def get_translation_t(t):
+#     """Get the translation matrix for movement in t."""
+#     matrix = [
+#         [1, 0, 0, 0],
+#         [0, 1, 0, 0],
+#         [0, 0, 1, t],
+#         [0, 0, 0, 1],
+#     ]
+#     return ops.convert_to_tensor(matrix, dtype="float32")
+
+
+# def get_rotation_phi(phi):
+#     """Get the rotation matrix for movement in phi."""
+#     matrix = [
+#         [1, 0, 0, 0],
+#         [0, ops.cos(phi), -ops.sin(phi), 0],
+#         [0, ops.sin(phi), ops.cos(phi), 0],
+#         [0, 0, 0, 1],
+#     ]
+#     return ops.convert_to_tensor(matrix, dtype="float32")
+
+
+# def get_rotation_theta(theta):
+#     """Get the rotation matrix for movement in theta."""
+#     matrix = [
+#         [ops.cos(theta), 0, -ops.sin(theta), 0],
+#         [0, 1, 0, 0],
+#         [ops.sin(theta), 0, ops.cos(theta), 0],
+#         [0, 0, 0, 1],
+#     ]
+#     return ops.convert_to_tensor(matrix, dtype="float32")
+
+
+# def pose_spherical(theta, phi, t):
+#     """
+#     Get the camera to world matrix for the corresponding theta, phi
+#     and t.
+#     """
+#     c2w = get_translation_t(t)
+#     c2w = get_rotation_phi(phi / 180.0 * np.pi) @ c2w
+#     c2w = get_rotation_theta(theta / 180.0 * np.pi) @ c2w
+#     c2w = np.array([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]) @ c2w
+#     return c2w
+
+# rgb_frames = []
+# batch_flat = []
+# batch_t = []
+
+# # Iterate over different theta value and generate scenes.
+# for index, theta in tqdm(enumerate(np.linspace(0.0, 360.0, 120, endpoint=False))):
+#     # Get the camera to world matrix.
+#     c2w = pose_spherical(theta, -30.0, 4.0)
+
+#     ray_oris, ray_dirs = get_rays(H, W, focal, c2w)
+
+#     rays_flat, t_vals = render_flat_rays(
+#         ray_oris, 
+#         ray_dirs, 
+#         near=2.0, 
+#         far=6.0, 
+#         num_samples=NUM_SAMPLES, 
+#         pos_encode_dims=POS_ENCODE_DIMS,
+#         rand=False
+#     )
+
+#     if index % BATCH_SIZE == 0 and index > 0:
+#         batched_flat = ops.stack(batch_flat, axis=0)
+#         batch_flat = [rays_flat]
+
+#         batched_t = ops.stack(batch_t, axis=0)
+#         batch_t = [t_vals]
+
+#         rgb, _ = render_rgb_depth(
+#             model=nerf_model,
+#             rays_flat=batched_flat,
+#             t_vals=batched_t,
+#             batch_size=BATCH_SIZE,
+#             h=H,
+#             w=W,
+#             num_samples=NUM_SAMPLES,
+#             rand=True,
+#             train=False
+#         )
+
+#         temp_rgb = [np.clip(255 * img, 0.0, 255.0).astype(np.uint8) for img in rgb]
+
+#         rgb_frames += temp_rgb
+#     else:
+#         batch_flat.append(rays_flat)
+#         batch_t.append(t_vals)
+
+
+# rgb_video = "rgb_video.mp4"
+# imageio.mimwrite(rgb_video, rgb_frames, fps=30, quality=7, macro_block_size=None)
