b61753cc10588d21bf94cf2e999008447d021f56
Muhammad Ghifary <mghifary@192.168.1.17>
2025-06-28 11:32:55 +0700

    clean up repo

--- inference_batch.py
+++ inference_batch.py
@@ -143,7 +143,8 @@ input_shape_for_build = (images_shape, rays_tuple_shape)
 nerf_trainer.build(input_shape=input_shape_for_build)
 
 # Load the model weights if they exist
-weight_path = f"{MODEL_DIR}/{config_filename}-best/nerf_l{NUM_LAYERS}_d{HIDDEN_DIM}_n{NS_COARSE + NS_FINE}_ep{EPOCHS}.weights.h5"
+# weight_path = f"{MODEL_DIR}/{config_filename}-best/nerf_l{NUM_LAYERS}_d{HIDDEN_DIM}_n{NS_COARSE + NS_FINE}_ep{EPOCHS}.weights.h5"
+weight_path = f"{MODEL_DIR}/{config_filename}-20250628-025644/nerf_l{NUM_LAYERS}_d{HIDDEN_DIM}_n{NS_COARSE + NS_FINE}_ep{EPOCHS}.weights.h5"
 if os.path.exists(weight_path):
     nerf_trainer.load_weights(weight_path)
     print("Model weights loaded successfully.")
@@ -151,7 +152,7 @@ else:
     print(f"Model weights not found at {weight_path}.")
 
 # Get first 5 samples from the validation dataset
-n_images = 1
+n_images = 3
 n_samples = n_images * H * W
 
 val_image_samples = val_images_s[:n_samples]
@@ -166,62 +167,62 @@ val_rgb_coarse, val_rgb_fine = val_rgbs
 val_depth_coarse, val_depth_fine = val_depths
 val_weights_coarse, val_weights_fine = val_weights
 
-# Debugging sample_pdf
-t_vals_mid = (0.5 * (t_vals[..., 1:] + t_vals[..., :-1]))
-weights = val_weights_coarse
+# # Debugging sample_pdf
+# t_vals_mid = (0.5 * (t_vals[..., 1:] + t_vals[..., :-1]))
+# weights = val_weights_coarse
 
-batch_size = ops.shape(weights)[0]
-if len(ops.shape(weights)) == 4: # (b, h, w, num_samples)
-    image_height, image_width = ops.shape(weights)[1:3]
+# batch_size = ops.shape(weights)[0]
+# if len(ops.shape(weights)) == 4: # (b, h, w, num_samples)
+#     image_height, image_width = ops.shape(weights)[1:3]
 
-weights += 1e-5
-# pdf = weights / tf.reduce_sum(weights, axis=-1, keepdims=True)
-pdf = weights / ops.sum(weights, axis=-1, keepdims=True)
-# cdf = tf.cumsum(pdf, axis=-1)
-cdf = ops.cumsum(pdf, axis=-1)
-# cdf = tf.concat([tf.zeros_like(cdf[..., :1]), cdf], axis=-1)
-cdf = ops.concatenate([ops.zeros_like(cdf[..., :1]), cdf], axis=-1)
+# weights += 1e-5
+# # pdf = weights / tf.reduce_sum(weights, axis=-1, keepdims=True)
+# pdf = weights / ops.sum(weights, axis=-1, keepdims=True)
+# # cdf = tf.cumsum(pdf, axis=-1)
+# cdf = ops.cumsum(pdf, axis=-1)
+# # cdf = tf.concat([tf.zeros_like(cdf[..., :1]), cdf], axis=-1)
+# cdf = ops.concatenate([ops.zeros_like(cdf[..., :1]), cdf], axis=-1)
 
-u_shape = [batch_size, NS_FINE]
+# u_shape = [batch_size, NS_FINE]
 
-# u = tf.random.uniform(shape=u_shape)
-u = keras.random.uniform(shape=u_shape)
-indices = tf.searchsorted(cdf, u, side="right")
+# # u = tf.random.uniform(shape=u_shape)
+# u = keras.random.uniform(shape=u_shape)
+# indices = tf.searchsorted(cdf, u, side="right")
 
-# def searchsorted_fn(cdf_row, u_row):
-#     return ops.searchsorted(cdf_row, u_row, side="right")
+# # def searchsorted_fn(cdf_row, u_row):
+# #     return ops.searchsorted(cdf_row, u_row, side="right")
 
-# indices = ops.vectorized_map(
-#     lambda args: searchsorted_fn(args[0], args[1]),
-#     (cdf, u)
-# )
+# # indices = ops.vectorized_map(
+# #     lambda args: searchsorted_fn(args[0], args[1]),
+# #     (cdf, u)
+# # )
 
-# below = tf.maximum(0, indices-1)
-below = ops.maximum(0, indices-1)
-# above = tf.minimum(cdf.shape[-1]-1, indices)
-above = ops.minimum(cdf.shape[-1]-1, indices)
-# indices_g = tf.stack([below, above], axis=-1)
-indices_g = ops.stack([below, above], axis=-1)
+# # below = tf.maximum(0, indices-1)
+# below = ops.maximum(0, indices-1)
+# # above = tf.minimum(cdf.shape[-1]-1, indices)
+# above = ops.minimum(cdf.shape[-1]-1, indices)
+# # indices_g = tf.stack([below, above], axis=-1)
+# indices_g = ops.stack([below, above], axis=-1)
 
-print(f"\n indices_g {indices_g.shape}: (min: {np.min(indices_g)}, max: {np.max(indices_g)})")
+# print(f"\n indices_g {indices_g.shape}: (min: {np.min(indices_g)}, max: {np.max(indices_g)})")
 
-cdf_g = tf.gather(cdf, indices_g, axis=-1, batch_dims=len(indices_g.shape)-2)
+# cdf_g = tf.gather(cdf, indices_g, axis=-1, batch_dims=len(indices_g.shape)-2)
 
-indices_gt = tf.minimum(t_vals_mid.shape[-1] - 1, indices_g)
-t_vals_mid_g = tf.gather(t_vals_mid, indices_gt, axis=-1, batch_dims=len(indices_g.shape)-2)
+# indices_gt = tf.minimum(t_vals_mid.shape[-1] - 1, indices_g)
+# t_vals_mid_g = tf.gather(t_vals_mid, indices_gt, axis=-1, batch_dims=len(indices_g.shape)-2)
 
-denom = cdf_g[..., 1] - cdf_g[..., 0]
-denom = tf.where(denom < 1e-5, tf.ones_like(denom), denom)
-t = (u - cdf_g[..., 0]) / denom
-samples = (t_vals_mid_g[..., 0] + t * 
-    (t_vals_mid_g[..., 1] - t_vals_mid_g[..., 0]))
+# denom = cdf_g[..., 1] - cdf_g[..., 0]
+# denom = tf.where(denom < 1e-5, tf.ones_like(denom), denom)
+# t = (u - cdf_g[..., 0]) / denom
+# samples = (t_vals_mid_g[..., 0] + t * 
+#     (t_vals_mid_g[..., 1] - t_vals_mid_g[..., 0]))
 
 
-# # Reshape the test_recons_images and depth_maps to (nb, H, W, 3) and (nb, H, W) respectively.
-# nb = int(ops.shape(val_image_samples)[0] / (H * W))
-# ori_imgs = ops.reshape(val_image_samples, (nb, H, W, 3))
-# recons_imgs = ops.reshape(val_rgb_fine, (nb, H, W, 3))
-# depth_maps = ops.reshape(val_depth_fine, (nb, H, W))
+# Reshape the test_recons_images and depth_maps to (nb, H, W, 3) and (nb, H, W) respectively.
+nb = int(ops.shape(val_image_samples)[0] / (H * W))
+ori_imgs = ops.reshape(val_image_samples, (nb, H, W, 3))
+recons_imgs = ops.reshape(val_rgb_fine, (nb, H, W, 3))
+depth_maps = ops.reshape(val_depth_fine, (nb, H, W))
 
 # # Create subplots
 # fig, axes = plt.subplots(nrows=n_images, ncols=3, figsize=(10, 20))
@@ -249,46 +250,46 @@ samples = (t_vals_mid_g[..., 0] + t *
 #     counter += 1
 # plt.show()
 
-# rgb_frames = []
-# batch_ray_oris = []
-# batch_ray_dirs = []
+rgb_frames = []
+batch_ray_oris = []
+batch_ray_dirs = []
 
-# # Iterate over different theta value and generate scenes.
-# for index, theta in tqdm(enumerate(np.linspace(0.0, 360.0, 120, endpoint=False))):
-#     # Get the camera to world matrix.
-#     c2w = pose_spherical(theta, -30.0, 4.0)
+# Iterate over different theta value and generate scenes.
+for index, theta in tqdm(enumerate(np.linspace(0.0, 360.0, 120, endpoint=False))):
+    # Get the camera to world matrix.
+    c2w = pose_spherical(theta, -30.0, 4.0)
 
-#     ray_oris, ray_dirs = get_rays(H, W, focal, c2w)
-#     # print(f"Shape of ray_oris: {ray_oris.shape}, ray_dirs: {ray_dirs.shape}")
+    ray_oris, ray_dirs = get_rays(H, W, focal, c2w)
+    # print(f"Shape of ray_oris: {ray_oris.shape}, ray_dirs: {ray_dirs.shape}")
 
-#     if index % 5 == 0 and index > 0:
-#         batched_ray_oris = ops.stack(batch_ray_oris, axis=0)
-#         batch_ray_oris = [ray_oris]
+    if index % 5 == 0 and index > 0:
+        batched_ray_oris = ops.stack(batch_ray_oris, axis=0)
+        batch_ray_oris = [ray_oris]
         
-#         batched_ray_dirs = ops.stack(batch_ray_dirs, axis=0)
-#         batch_ray_dirs = [ray_dirs]
+        batched_ray_dirs = ops.stack(batch_ray_dirs, axis=0)
+        batch_ray_dirs = [ray_dirs]
 
-#         # Render the RGB and depth maps using the nerf model
-#         ray_ori_s = ops.reshape(batched_ray_oris, (-1, batched_ray_oris.shape[-1]))
-#         ray_dir_s = ops.reshape(batched_ray_dirs, (-1, batched_ray_dirs.shape[-1]))
-#         t_vals_s = generate_t_vals(near, far, ops.shape(ray_ori_s)[0], NS_COARSE, rand_sampling=False)
+        # Render the RGB and depth maps using the nerf model
+        ray_ori_s = ops.reshape(batched_ray_oris, (-1, batched_ray_oris.shape[-1]))
+        ray_dir_s = ops.reshape(batched_ray_dirs, (-1, batched_ray_dirs.shape[-1]))
+        t_vals_s = generate_t_vals(near, far, ops.shape(ray_ori_s)[0], NS_COARSE, rand_sampling=False)
 
-#         rgbs, depth_maps, weights, _ = nerf_trainer.forward_pass(ray_ori_s, ray_dir_s, t_vals_s, L_XYZ, L_DIR, training=False)
+        rgbs, depth_maps, weights, _ = nerf_trainer.forward_pass(ray_ori_s, ray_dir_s, t_vals_s, L_XYZ, L_DIR, training=False)
 
-#         # Get the RGB from the fine model
-#         rgb_fine = rgbs[1]
+        # Get the RGB from the fine model
+        rgb_fine = rgbs[1]
 
-#         # Reshape rgb_fine to (nb, H, W, 3)
-#         nb = int(ops.shape(rgb_fine)[0] / (H * W))
-#         rgb_fine = ops.reshape(rgb_fine, (nb, H, W, 3))
+        # Reshape rgb_fine to (nb, H, W, 3)
+        nb = int(ops.shape(rgb_fine)[0] / (H * W))
+        rgb_fine = ops.reshape(rgb_fine, (nb, H, W, 3))
         
-#         # Get the RGB frames from the rendered images
-#         temp_rgb = [np.clip(255 * img, 0.0, 255.0).astype(np.uint8) for img in rgb_fine]
-#         rgb_frames += temp_rgb
-#     else:
-#         # batch_flat.append(rays_flat)
-#         batch_ray_oris.append(ray_oris)
-#         batch_ray_dirs.append(ray_dirs)
+        # Get the RGB frames from the rendered images
+        temp_rgb = [np.clip(255 * img, 0.0, 255.0).astype(np.uint8) for img in rgb_fine]
+        rgb_frames += temp_rgb
+    else:
+        # batch_flat.append(rays_flat)
+        batch_ray_oris.append(ray_oris)
+        batch_ray_dirs.append(ray_dirs)
         
-# rgb_video = f"{config_filename}_rgb_video_v2.mp4"
-# imageio.mimwrite(rgb_video, rgb_frames, fps=30, quality=9, macro_block_size=None)
+rgb_video = f"{config_filename}_rgb_video_tpu.mp4"
+imageio.mimwrite(rgb_video, rgb_frames, fps=30, quality=9, macro_block_size=None)
